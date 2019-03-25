b1128f2391b407ec80854c5ec6943212ddedb78e
gschurck <g.schurck@gmail.com>
2019-03-25 16:30:40 +0100

    First commit

---
+++ transfer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+
+# Le Raspberry Pi affiche à l'écran ce que lui envoie l'Arduino 
+# http://electroniqueamateur.blogspot.com/2014/05/communication-par-usb-entre-raspberry.html
+
+import serial
+ser = serial.Serial('/dev/ttyACM0', 9600)
+while 1 :
+    print(ser.readline())
