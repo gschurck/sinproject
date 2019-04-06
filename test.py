44bab70324319c48a33afb837f5406ca160da62c
Guillaume <g.schurck@gmail.com>
2019-04-07 00:21:11 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,9 +3,14 @@
 
 # Le Raspberry Pi affiche à l'écran ce que lui envoie l'Arduino 
 # http://electroniqueamateur.blogspot.com/2014/05/communication-par-usb-entre-raspberry.html
-
+##########################################
+"""
 import serial
-ser = serial.Serial('/dev/ttyACM0', 9600)
+
+
+#ser = serial.Serial('/dev/ttyACM0', 9600)
+"""
+##########################################
 while 1 :
     #print(ser.readline())#python 2.7.15
     #ch = ser.readline()
