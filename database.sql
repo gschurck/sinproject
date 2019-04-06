0568133044fe5b04b4c5604c511ae00bb7583a28
Guillaume <g.schurck@gmail.com>
2019-04-07 01:25:43 +0200

    update

---
+++ database.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE elevage CHARACTER SET 'utf8';
--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: latin-1 -*-
 
-# Le Raspberry Pi affiche à l'écran ce que lui envoie l'Arduino 
+# Le Raspberry Pi affiche Ã  l'Ã©cran ce que lui envoie l'Arduino 
 # http://electroniqueamateur.blogspot.com/2014/05/communication-par-usb-entre-raspberry.html
+import time
 ##########################################
 """
 import serial
@@ -11,23 +12,29 @@ import serial
 #ser = serial.Serial('/dev/ttyACM0', 9600)
 """
 ##########################################
+
 while 1 :
     #print(ser.readline())#python 2.7.15
     #ch = ser.readline()
     #print(ch)
-    ch = "bonjour"
+    ch = "Temperature : 6"
     
    #"""
     if ch[0]=="T":  
         # SQL Temperature
-        print(ch[5:7])
-        print("t")
+        temperature=ch[14:15]
+        print("Temperature :")
+        #print(ch[5:7])
+        print(temperature)
+        time.sleep(1)
     elif ch[0]=="H":
         # SQL Humidity
+        humidity=ch[5:7]
         print(ch[5:7])
         print("h")
     elif ch[0]=="b":
         # SQL Humidity
+        humidity=ch[5:7]
         print(ch[5:7])
         print("b")
     #"""       
